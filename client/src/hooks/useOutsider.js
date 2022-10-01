029f1a047ae517b0592b556a137f719d6600eb76
Kajal Kumari <kajal.kumari@happay.com>
2022-10-02 01:40:22 +0530

    fix removed commented code

--- client/src/hooks/useOutsider.js
+++ client/src/hooks/useOutsider.js
@@ -1,18 +1,14 @@
-import React, { useRef, useEffect } from "react";
+import React, {useEffect } from "react";
 export function useOutside(ref, setNote) {
   useEffect(() => {
-    /**
-     * Alert if clicked on outside of element
-     */
     function handleClickOutside(event) {
       if (ref.current && !ref.current.contains(event.target)) {
         setNote(false);
       }
     }
-    // Bind the event listener
+
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      // Unbind the event listener on clean up
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [ref]);
